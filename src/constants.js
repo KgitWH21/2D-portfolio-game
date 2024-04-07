ae17fdc8fc006b7ac295d4bcfaad3de5cafa3a44
Keith Hayden <kwhayden11@gmail.com>
2024-04-07 16:13:53 +0900

    Initial project commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is Keith's PC, where he writes, makes graphics, music and more. Read his <a href="https://keithhayden.net/blog/" target="_blank_">blog</a> here.`,
+    library: `On the bookshelf there are 5 works of fiction, all written by Keith. Browse the <a href="https://keithhayden.net/keith-fiction/" target="_blank_">titles</a> here.`,
+    resume: `Among the papers is a work in progress. A military game tech fiction short story called "Sync-Whole". You can learn more on Keith's <a href="https://keithhayden.net/" target="_blank_">website.</a>`, 
+    "cs-degree": `It says: "I will help you write a book!" - Keith. Go <a href="https://keithhayden.net/book-writing-tools/" target="_blank_">here</a> to learn more.`,
+    bed: `What's this? Even on the bed there's a stack of 4 non-fiction books. (When does he sleep?) On top is his military autobiography "Hayden: Brave". Look closer <a href="https://keithhayden.net/nonfiction/" target="_blank_">here.</a>`,
+    projects: `Looks like Keith's offering 1-on-1 creative consultation on limited basis. (Better get on that before it fills up.) It says: "Read for 
+    <a href="https://keithhayden.net/gifted-book-writer-program/" target="_blank_">details</a>."`,
+    "sofa-table": `A note from Keith: "To teach Japanese, I created "Tower of Babbling Japanese." A resource designed for complete beginners to learn the language from scratch.
+       It is my hope this free guide allows others to enjoy Japanese." He just left it out <a href="https://keithhayden.net/tower-of-babbling-japanese/" target="_blank_">here.</a>`,
+    tv: `It's the TV. There are 4 channels: 1) <a href="https://youtu.be/_DhX-6EV2UQ" target="_blank_">music</a>
+    2) <a href="https://militarygametechfiction.carrd.co/" target="_blank_">military game tech fiction</a>
+    3) <a href="https://militarygametechfiction.carrd.co/" target="_blank_">Cereus & Limnic</a>
+    4) <a href="https://cereus-limnic-part-one-jp.carrd.co/" target="_blank_">セレウス&リムニク</a>. The rest is static.`, 
+    exit: `A sign on the door reads: To exit Keith's office close the tab. What did you create today? Nothing? Get busy! - Keith`,
+    
+
+
+
+}
